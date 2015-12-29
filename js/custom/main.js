5a116affb4ec9cad591d72399b85d9ce41fd3fb1
Subhash Mahimaluri <subhash_mahimaluri@mindtree.com>
2015-12-29 12:21:18 +0530

    Edited-on-29thDec2016

--- js/custom/main.js
+++ js/custom/main.js
@@ -5,6 +5,24 @@
 
             // Header fixed with admin toolbar
             $(function() {
+                // Add Body class on view port change
+                assign_bootstrap_mode();
+                $(window).resize(function() {
+                    assign_bootstrap_mode();
+                });
+                function assign_bootstrap_mode() {
+                    width = window.innerWidth;
+                    var mode = '';
+                    if (width<1024) {
+                        mode = "mode-md";
+                    }
+                    else if (width>1023) {
+                        mode = "mode-lg";                        
+                    }
+                    $("body").removeClass("mode-md").removeClass("mode-lg").addClass(mode);
+                }
+
+                // Search Functionality
                 $('.search-icon a').click(function(e) {
                     e.preventDefault();
                     $('#search-wrapper').show();
@@ -17,15 +35,29 @@
                 $("#search-wrapper").focusout(function() {
                     $('#search-wrapper').hide();
                 });
+                // Mobile Dropdown Event
+                $('.dropdown-indicator').click(function(){
+                    $(this).parent().find('> ul').toggle();
+                    $(this).toggleClass('open');
+                });
+
+                // Dropdown Menu                
                 $('nav ul li').each(function() {
                     if ($(this).find('> ul').length > 0) {
                         $(this).addClass('has-dropdown');
-                        $(this).find('> a').append('<span class="dropdown-indicator"><i class="fa fa-angle-down"></i></span>');
+                        $(this).prepend('<span class="dropdown-indicator"><i class="fa fa-angle-down"></i></span>');
                     }
                 });
-                $('.navbar-collapse.in ul li a .dropdown-indicator').click(function(event){
-                    event.preventDefault();
-                    alert('clickedd');
+                $("a[href='#top']").on('click', function() {
+                  $("html, body").animate({ scrollTop: 0 }, 1000);
+                  return false;
+                });                
+                $(window).scroll(function() {      
+                    if ($(window).scrollTop() >= 100 ) {
+                      $("#navbar, .main-container").addClass("shrink");
+                    } else{
+                      $("#navbar, .main-container").removeClass("shrink");
+                    }
                 });
             });
         }
